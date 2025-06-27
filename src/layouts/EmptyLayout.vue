67b49b21a237cd038a670a7bd38371c0c3e21daa
Jehu-Salazar <irrabalpul@gmail.com>
2025-06-27 11:44:53 -0500

    cambios de sesion

---
+++ src/layouts/EmptyLayout.vue
@@ -0,0 +1,17 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script setup>
+// Layout vacío para páginas sin header/footer
+</script>
+
+<style scoped>
+.q-layout {
+  min-height: 100vh;
+}
+</style>
